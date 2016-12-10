a0f9537c7274948eaf2ef2105163e9f3c6e885e0
chee <chee@snaek.org>
2016-12-10 00:35:23 +0000

    also copy it into the clipboard

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ depends on:
 
 * xdotool
 * rofi
+* xsel
 
 offers you a list of all the emoji and then when you
 pick one it types it into the focused input 4 u
+
+it also now copies it to the clipboard because xdotool
+doesn't support printing emoji into some windows
 
 install it by
 
--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const exec = require('child_process').exec
 
 exec(
   `echo '${list.data.map(item => `${item[1]} ${item[2].toLowerCase()} (${item[3].replace(/\| /g, '')})`).join('\n')}' | rofi -dmenu`,
-  (error, choice) => error
-    ? console.error(error)
-    : exec(`xdotool type ' ${choice.split(' ')[0]}'`)
+  (error, choice) => {
+    if (error) throw error
+    const emoji = choice.split(' ')[0]
+    exec(`xdotool type ' ${emoji}'`)
+    exec(`echo -n ${emoji} | xsel -b`)
+  }
 )
